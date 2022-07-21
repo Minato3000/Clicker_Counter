ad97d6e20491c99b59ed7f8c6ed2d8596e62019e
Raghuram <sraghu979@gmail.com>
2022-07-21 18:33:38 +0530

    updated class to function

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import './App.css';
 
-class App extends Component {
-  state = {
-    count: 0
+function App() {
+  const [count, updateCount] = useState(0);
+
+  const increament = () => {
+    updateCount(count + 1);
   };
 
-  increament = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  };
-  
-  render() {
-    return (
-      <div className='clickerbox'>
-        <h1>Clicker Counter</h1>
-        <div className='buttondiv'>
-          <button onClick={this.increament}> Click me </button>
-        </div>
-        <p>You have clicked me {this.state.count} times</p>
+  return (
+    <div className='clickerbox'>
+      <h1>Clicker Counter</h1>
+      <div className='buttondiv'>
+        <button onClick={increament}> Click me </button>
       </div>
-    );
-  }
+      <p>You have clicked me {count} times</p>
+    </div>
+  );
 }
+
+// class App extends Component {
+//   state = {
+//     count: 0
+//   };
+
+//   increament = () => {
+//     this.setState({
+//       count: this.state.count + 1
+//     });
+//   };
+  
+//   render() {
+//     return (
+//       <div className='clickerbox'>
+//         <h1>Clicker Counter</h1>
+//         <div className='buttondiv'>
+//           <button onClick={this.increament}> Click me </button>
+//         </div>
+//         <p>You have clicked me {this.state.count} times</p>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
